2372dc7ee320f90f6d6a64ec26798b9941f17110
Michele Scarlato <michele.scarlato@gmail.com>
2021-08-10 11:11:21 +0900

    importing common lists from another python module.

--- src/LCV/LCVlib/VerboseLicenseParsing.py
+++ src/LCV/LCVlib/VerboseLicenseParsing.py
@@ -8,17 +8,13 @@ import pandas as pd
 import numpy as np
 import re
 import csv
+from LCVlib.CommonLists import *
 
 '''
 * SPDX-FileCopyrightText: 2021 Michele Scarlato <michele.scarlato@endocode.com>
 *
 * SPDX-License-Identifier: MIT
 '''
-
-licenses = ["AFL", "AGPL", "Apache", "Artistic", "BSD", "BSL", "bzip2", "CC0", "CDDL", "CPE", "CPL", "curl", "EFL", "EPL", "EUPL", "FTL", "GPL", "HPND", "IBM", "ICU", "IJG", "IPL", "ISC",
-            "LGPL", "Libpng", "libtiff", "MirOS", "MIT", "CMU", "MPL", "MS", "NBPL", "NTP", "OpenSSL", "OSL", "Python", "Qhull", "RPL", "SunPro", "Unicode", "UPL", "WTFPL", "X11", "XFree86", "Zlib", "zlib-acknowledgement"]
-versions = ["1.0", "1.0.5", "1.0.6", "1.1", "1.2", "1.5",
-            "2.0", "2.1", "3.0", "3.1", "1", "2", "3", "4", "5"]
 
 def DetectWithAcronyms(verbose_license):
     licenseVersion = None
@@ -73,12 +69,6 @@ def DetectWithKeywords(verbose_license):
     supposedLicense = None
     orLater = False
     only = False
-    DynamicMappingKeywordsList = [
-        "2010", "2014", "academic", "affero", "attribution", "berkeley", "bsd", "bzip", "classpath", "clear", "cmu", "cpe", "commons", "creative", "database", "distribution", "eclipse", "epl", "eupl", "european",
-        "exception","expat", "general", "ibm", "later", "lesser","lgpl", "libpng", "library", "license", "miros", "mozilla", "modification", "mpi", "mpl", "ntp", "new", "nuclear", "national", "only", "open", "openssl", "patent", "psf","psfl", "python",
-        "png", "power", "powerpc", "public", "permissive", "qhull", "reciprocal", "shortened","simplified","software", "tiff", "uc", "universal",
-        "upl", "views", "warranty", "zlib", "zero",]
-
     MappedKeywords = list()
     list_of_words = verbose_license.split()
     for word in list_of_words:
