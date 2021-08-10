35aa675647c75e9ebd3aa15dcb7ba048f15be633
Michele Scarlato <michele.scarlato@gmail.com>
2021-08-10 17:33:58 +0900

    Added dictionary mapping literals version numbers to digit

--- src/LCV/LCVlib/CommonLists.py
+++ src/LCV/LCVlib/CommonLists.py
@@ -17,8 +17,23 @@ licenses = ["AFL", "AGPL", "Apache", "Artistic", "BSD", "BSL", "bzip2", "CC0", "
             "LGPL", "Libpng", "libtiff", "MirOS", "MIT", "CMU", "MPL", "MS", "NBPL", "NTP", "OpenSSL", "OSL", "Python", "Qhull", "RPL", "SunPro", "Unicode", "UPL", "WTFPL", "X11", "XFree86", "Zlib", "zlib-acknowledgement"]
 versions = ["1.0", "1.0.5", "1.0.6", "1.1", "1.2", "1.5",
             "2.0", "2.1", "3.0", "3.1", "1", "2", "3", "4", "5"]
+literalVersions = ["one","two","three","four","five"]
+
 DynamicMappingKeywordsList = [
-    "2010", "2014", "academic", "affero", "attribution", "berkeley", "bsd", "bzip", "classpath", "clear", "cmu", "cpe", "commons", "creative", "database", "distribution", "eclipse", "epl", "eupl", "european",
-    "exception","expat", "general", "ibm", "later", "lesser","lgpl", "libpng", "library", "license", "miros", "mozilla", "modification", "mpi", "mpl", "ntp", "new", "nuclear", "national", "only", "open", "openssl", "patent", "psf","psfl", "python",
+    "2010", "2014", "academic", "affero", "attribution", "berkeley", "bsd", "bzip", "classpath", "clear", "cmu", "cpe", "commons", "creative",
+    "database", "distribution", "eclipse", "epl", "eupl", "european",
+    "exception","expat", "general", "ibm", "later", "lesser","lgpl", "libpng", "library", "license", "miros", "mozilla", "modification", "mpi",
+    "mpl", "ntp", "new", "nuclear", "national", "only", "open", "openssl", "patent", "psf","psfl", "python",
     "png", "power", "powerpc", "public", "permissive", "qhull", "reciprocal", "shortened","simplified","software", "tiff", "uc", "universal",
-    "upl", "views", "warranty", "zlib", "zero",]
+    "upl", "views", "warranty", "zlib", "zero","AFL", "AGPL", "Apache", "Artistic", "BSD", "BSL", "bzip2", "CC0", "CDDL", "CPE", "CPL", "curl",
+    "EFL", "EPL", "EUPL", "FTL", "GPL", "HPND", "IBM", "ICU", "IJG", "IPL", "ISC", "LGPL", "Libpng", "libtiff", "MirOS", "MIT", "CMU", "MPL",
+    "MS", "NBPL", "NTP", "OpenSSL", "OSL", "Python", "Qhull", "RPL", "SunPro", "Unicode",
+    "UPL", "WTFPL", "X11", "XFree86", "Zlib", "zlib-acknowledgement"]
+
+NumberDict = {
+  "one": "1",
+  "two": "2",
+  "three": "3",
+  "four": "4",
+  "five": "5"
+}
