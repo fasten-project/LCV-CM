fb356008bd9b1f2d7d97b9bdbbd32cb37f0c4786
Michele Scarlato <michele.scarlato@gmail.com>
2021-08-24 15:21:11 +0900

    setting starting line and ending line to low numbers (useful to setup the scraplin environment).

--- src/LCV/PYPI_license_collector.py
+++ src/LCV/PYPI_license_collector.py
@@ -68,8 +68,8 @@ def appendToFile(license):
         # Append text at the end of file
         file_object.write(license)
 
-startLine= 154974
-endLine=   155000
+startLine= 0
+endLine=   10
 with open('whole_pypi_package_list.txt') as f:
     packages=[]
     packages_unstripped = f.readlines()[startLine:endLine]
