5c914a96f7b6bcfac4e3a470e8aa140d78c9af07
Michele Scarlato <michele.scarlato@gmail.com>
2021-08-30 09:40:39 +0900

    list of not recognizable or already fixed updated with the last 40 000 packages collected (still WIP).

--- src/LCV/PYPI_log_parser.py
+++ src/LCV/PYPI_log_parser.py
@@ -27,13 +27,20 @@ excluded_not_converted= ["ASL","GPL","LGPL","AGPL", "AFL", "GNU", "Creative Comm
 "License", "LICENCE","LICENSE","LICENSE.txt","Dual License", "Proprietary", "todo","TODO",
 "Artistic","ZPL","ASK FOR PERMISSIONS","Public Domain", "Private","Proprietary License",
 "PSL", "All Rights Reserved", "WPI", "CeCILL","OSI","LUMS", "UCSD", "EUPL" , "EULA", "CDDL",
-"commercial","undefined","CC-BY-SA"]
+"commercial","undefined","CC-BY-SA", "Free", "Freeware", "EPL", "RPL",
+]
 
 already_fixed= ["GNU v3", "MIT;", "CC0","APL", "CeCILL-B", "zlib",
 "CC BY-SA 3.0", "CC by 4.0", "EUPL v1.2 or later", "BEERWARE",
-"cc-by-sa-4.0", "beer", "ISCL", "Attribution-NonCommercial-ShareAlike 4.0 International", "Attribution-ShareAlike 4.0 International CC BY-SA 4.0",
+"cc-by-sa-4.0", "beer", "ISCL",
+"Attribution-NonCommercial-ShareAlike 4.0 International",
+"Attribution-ShareAlike 4.0 International CC BY-SA 4.0", "CC BY-SA 4.0",
 "Attribution-NonCommercial-NoDerivatives 4.0 International CC BY-NC-ND 4.0",
-"CC BY-NC-SA 4.0", "CCSA-4.0", "CC BY-NC 4.0","IBM", "wxWindows","CC BY-NC-ND 4.0",]
+"CC BY-NC-SA 4.0", "CCSA-4.0", "CC BY-NC 4.0","IBM",
+"wxWindows","CC BY-NC-ND 4.0","ICS",
+"https://creativecommons.org/publicdomain/zero/1.0/",
+"SIL OFL 1.1",
+]
 
 
 
