01d41412b0796cab246cd07792ed5ce4bd69c638
Michele Scarlato <michele.scarlato@gmail.com>
2021-09-11 10:12:10 +0900

    dummy main for a new output

---
+++ src/LCV/dummy_main_restAPIs_mod.py
@@ -0,0 +1,43 @@
+import sys
+from LCVlib.testlistsGithubAPI import GitHubURLList
+from LCVlib.testlistsJSONfiles import JSONPathList
+# from LCVlib.verify import retrieveOutboundLicense, CheckOutboundLicense
+# from LCVlib.verify import RetrieveInboundLicenses, Compare, CompareFlag
+import requests
+import json
+import time
+import sys
+import pandas as pd
+import numpy as np
+import re
+from LCVlib.SPDXIdMapping import StaticMappingList, IsAnSPDX, StaticMapping, DynamicMapping, IsInAliases, ConvertToSPDX
+from LCVlib.CheckAliasAndSPDXId import *
+from LCVlib.CommonLists import *
+from LCVlib.VerboseLicenseParsing import *
+from LCVlib.verify import CSV_to_dataframeOSADL
+
+
+'''
+* SPDX-FileCopyrightText: 2021 Michele Scarlato <michele.scarlato@endocode.com>
+*
+* SPDX-License-Identifier: MIT
+'''
+
+license = "GPL99"
+OutboundLicense = "Diesel95"
+#dict.fromkeys([message, status, inbound, outbound])
+keys = ["message","status","inbound","outbound"]
+dictOutput = dict.fromkeys(keys, None)
+print(dictOutput)
+
+
+output = license+" is not compatible with " + \
+    OutboundLicense+" as an outbound license"
+
+dictOutput['message'] = output
+dictOutput['status'] = "not compatible"
+dictOutput['inbound'] = license
+dictOutput['outbound'] = OutboundLicense
+
+print(dictOutput)
+#print(dictOutput)
