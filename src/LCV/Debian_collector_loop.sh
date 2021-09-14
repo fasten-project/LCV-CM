724686b76f6ea4c1699ab9cc9c6e38afff3f930b
Michele Scarlato <michele.scarlato@gmail.com>
2021-09-14 09:07:20 +0900

    removing logs from the loop

--- src/LCV/Debian_collector_loop.sh
+++ src/LCV/Debian_collector_loop.sh
@@ -3,7 +3,8 @@ hs=`hostname`
 
 for i in {0..10}
 do
-  python3 Debian_license_collector.py > logs_debian/$hs/"$(date +"%Y_%m_%d_%I_%M").log"
+  #python3 Debian_license_collector.py > logs_debian/$hs/"$(date +"%Y_%m_%d_%I_%M").log"
+  python3 Debian_license_collector.py
   echo "Running $i cycle"
   sleep 100
 done
